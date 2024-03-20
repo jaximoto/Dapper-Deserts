626d23cf7e2d77931491a5e40430280b2c197886
jaximoto <62913915+jaximoto@users.noreply.github.com>
2024-03-20 11:40:28 -0700

    Prototype popout ham menu

--- src/app/hamburger.tsx
+++ src/app/hamburger.tsx
@@ -10,28 +10,35 @@ export default function Hamburger()
         setIsOpen(!isOpen);
     }
     return(
-        <button onClick={handleClick}
-        className='fixed flex flex-col justify-center
-        items-center gap-y-1 h-7 w-7 bg-slate-100 rounded-sm'>
-            <span className={` bg-stone-800 block transition-all duration-300 ease-out 
-                    h-1 w-6 rounded-sm
-                    ${isOpen ? 
-                        'rotate-45 translate-y-2' : '-translate-y-0.5'
-                        }`}>
+        <div className='fixed'>
+            <button onClick={handleClick}
+            className='flex flex-col justify-center
+            items-center gap-y-1 h-7 w-7 bg-slate-100 rounded-sm'>
+                <span className={` bg-stone-800 block transition-all duration-300 ease-out 
+                        h-1 w-6 rounded-sm
+                        ${isOpen ? 
+                            'rotate-45 translate-y-2' : '-translate-y-0.5'
+                            }`}>
 
-            </span>
-            <span className={` bg-stone-800 block transition-all duration-300 ease-out 
-                    h-1 w-6 rounded-sm ${isOpen ? 
-                        'opacity-0' : 'opacity-100'
-                        }`}>
+                </span>
+                <span className={` bg-stone-800 block transition-all duration-300 ease-out 
+                        h-1 w-6 rounded-sm ${isOpen ? 
+                            'opacity-0' : 'opacity-100'
+                            }`}>
 
-                    </span>
-            <span className={` bg-stone-800 block transition-all duration-300 ease-out 
-                    h-1 w-6 rounded-sm ${isOpen ? 
-                        '-rotate-45 -translate-y-2' : 'translate-y-0.5'
-                        }`}>
+                        </span>
+                <span className={` bg-stone-800 block transition-all duration-300 ease-out 
+                        h-1 w-6 rounded-sm ${isOpen ? 
+                            '-rotate-45 -translate-y-2' : 'translate-y-0.5'
+                            }`}>
 
-                    </span>
-        </button>
+                        </span>
+            </button>
+            <div className={`bg-slate-100 h-screen w-96 ${isOpen ? 'flex' : 'hidden'}`}>
+                            
+
+            </div>
+        </div>
+        
     )
 }
