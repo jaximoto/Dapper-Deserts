7fd2208e5214ce793160a6036a1a55deedc89def
jaximoto <62913915+jaximoto@users.noreply.github.com>
2024-03-25 11:10:15 -0700

    Added Node mailer and created order form prototype

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,6 +1,48 @@
+"use client"
+import React, {useState} from "react"
+
+interface FormData {
+    name: string;
+    email: string;
+}
 export default function Order()
 {
+    const [formData, setFormData] = useState<FormData>({
+        name: '',
+        email:'',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        // Perform processing
+        console.log('Form submitted:', formData);
+    };
+
     return(
-    <h1>Order</h1>
+        <div>
+            <h1>Orders</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" name="name" value={formData.name} onChange={handleChange} required/>
+                </label>
+                
+                
+                <label>
+                    Email:
+                    <input type="email" name="email" value={formData.email} onChange={handleChange} required/>
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    
     )
 }
